eee19f8f2da4b28d25f1d9a6decca1f785ac436e
youngrock1 <proger1311@gmail.com>
2026-01-21 22:27:19 +0300

    Update src/keyboards/reply.py add general keyboards

---
+++ src/keyboards/reply.py
@@ -0,0 +1,19 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+main_kb = ReplyKeyboardMarkup(
+
+    keyboard=
+    [
+        [KeyboardButton(text='Смотреть анкеты'), KeyboardButton(text='Заполнить анкету заново'), KeyboardButton(text='Статистика')]
+    ],
+    one_time_keyboard=True
+)
+
+ank_kb = ReplyKeyboardMarkup(
+    keyboard=
+    [
+        [KeyboardButton(text='Лайк'), KeyboardButton(text='Написать'), KeyboardButton(text='Дизлайк'), KeyboardButton(text='Отключить анкету')]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Выбери опцию ниже"
+)
