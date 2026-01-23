8c8738139eed64c44c489d603bf329986f3aca0d
youngrock1 <proger1311@gmail.com>
2026-01-23 21:42:33 +0300

    Update src/handlers/bot_messages.py

--- src/handlers/bot_messages.py
+++ src/handlers/bot_messages.py
@@ -1,3 +1,22 @@
+from aiogram.types import Message
 from aiogram import Router, F
 
 router = Router() # Инифциализация роутера
+
+
+@router.message(F.text)
+async def handler_text(message: Message):
+    if message.text == 'Смотреть анкеты 🔥':
+        pass
+    elif message.text == 'Заполнить анкету заново 🔄':
+        pass
+    elif message.text == 'Статистика 📊':
+        pass
+    elif message.text == 'Лайк 👍':
+        pass
+    elif message.text == 'Написать ✍️':
+        pass
+    elif message.text == 'Дизлайк 👎':
+        pass
+    elif message.text == 'Отключить анкету 💤':
+        pass
