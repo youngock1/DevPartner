4769fb287210f7f78d1ec23bee2c0ecafafa5aba
youngrock1 <proger1311@gmail.com>
2026-01-23 21:37:17 +0300

    Update src/keyboards/reply.py

--- src/keyboards/reply.py
+++ src/keyboards/reply.py
@@ -4,7 +4,7 @@ main_kb = ReplyKeyboardMarkup(
 
     keyboard=
     [
-        [KeyboardButton(text='Смотреть анкеты'), KeyboardButton(text='Заполнить анкету заново'), KeyboardButton(text='Статистика')]
+        [KeyboardButton(text='Смотреть анкеты 🔥'), KeyboardButton(text='Заполнить анкету заново 🔄'), KeyboardButton(text='Статистика 📊')]
     ],
     one_time_keyboard=True,
     resize_keyboard=True
@@ -13,7 +13,7 @@ main_kb = ReplyKeyboardMarkup(
 ank_kb = ReplyKeyboardMarkup(
     keyboard=
     [
-        [KeyboardButton(text='Лайк'), KeyboardButton(text='Написать'), KeyboardButton(text='Дизлайк'), KeyboardButton(text='Отключить анкету')]
+        [KeyboardButton(text='Лайк 👍'), KeyboardButton(text='Написать ✍️'), KeyboardButton(text='Дизлайк 👎'), KeyboardButton(text='Отключить анкету 💤')]
     ],
     resize_keyboard=True,
     input_field_placeholder="Выбери опцию ниже"
