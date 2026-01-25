725a70189993b703e9fd0c2e408f6514ddc0800a
youngrock1 <proger1311@gmail.com>
2026-01-25 23:53:41 +0300

    Update many files

--- src/handlers/user_commands.py
+++ src/handlers/user_commands.py
@@ -1,10 +1,23 @@
 from aiogram.filters import CommandStart, Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 from keyboards import reply
 from aiogram.types import Message
 from aiogram import Router, F
+from database import test
+import datetime
 
 
 router = Router() # Инициализация роутера
+
+
+class Form_anket(StatesGroup):
+    full_name = State()
+    age = State()
+    photo = State()
+    stack = State()
+    city = State()
+    about_self = State()
 
 
 @router.message(CommandStart())
@@ -15,3 +28,57 @@ async def start_command(message: Message):
 @router.message(Command('help'))
 async def help_command(message: Message):
     await message.answer(f"Руководство бота...")
+
+
+@router.message(Command('registr'))
+async def registration_command(message: Message, state: FSMContext):
+    await message.answer("Введите ваше имя и фамилию...")
+    await state.set_state(Form_anket.full_name)
+
+
+@router.message(Form_anket.full_name)
+async def get_full_name(message: Message, state: FSMContext):
+    await state.update_data(full_name=message.text)
+    await state.set_state(Form_anket.age)
+    await message.answer("Введите свой возраст...")
+
+
+@router.message(Form_anket.age)
+async def get_age(message: Message, state: FSMContext):
+    if message.text.isdigit():
+        await state.update_data(age=message.text)
+        await state.set_state(Form_anket.photo)
+        await message.answer("Теперь скинь фото...")
+    else:
+        await message.answer("Введите свой возраст...")
+
+
+@router.message(Form_anket.photo, F.photo)
+async def get_photo(message: Message, state: FSMContext):
+    photo_file_id = message.photo[-1].file_id
+    await state.update_data(photo=photo_file_id)
+    await state.set_state(Form_anket.stack)
+    await message.answer("Введите свой Stack")
+
+
+@router.message(Form_anket.stack)
+async def get_stack(message: Message, state: FSMContext):
+    await state.update_data(stack=message.text)
+    await state.set_state(Form_anket.city)
+    await message.answer("Теперь введите свой город")
+
+
+@router.message(Form_anket.city)
+async def get_city(message: Message, state: FSMContext):
+    await state.update_data(city=message.text)
+    await state.set_state(Form_anket.about_self)
+    await message.answer("Теперь расскажите о себе...")
+
+
+@router.message(Form_anket.about_self)
+async def get_about_self(message: Message, state: FSMContext):
+    await state.update_data(about_self=message.text)
+    data = await state.get_data()
+    await state.clear()
+    test.create_user(id=message.from_user.id, full_name=data["full_name"], photo=str(data["photo"]), stack=data["stack"], city=data["city"],
+                              registration_date=datetime.datetime.now().strftime("%Y-%m-%d    %H:%M:%S"), about_self=data["about_self"], like=None)
