394bb880f8b71c34949150823ce21717c5526903
youngrock1 <proger1311@gmail.com>
2026-01-25 14:05:50 +0300

    Update many files

--- src/handlers/user_commands.py
+++ src/handlers/user_commands.py
@@ -1,4 +1,5 @@
 from aiogram.filters import CommandStart, Command
+from keyboards import reply
 from aiogram.types import Message
 from aiogram import Router, F
 
@@ -8,4 +9,9 @@ router = Router() # Инициализация роутера
 
 @router.message(CommandStart())
 async def start_command(message: Message):
-    await message.answer(f"Добро пожаловать в бота, {message.from_user.full_name}!")
+    await message.answer(f"Добро пожаловать в бота, {message.from_user.full_name}!", reply_markup=reply.main_kb)
+
+
+@router.message(Command('help'))
+async def help_command(message: Message):
+    await message.answer(f"Руководство бота...")
--- src/keyboards/reply.py
+++ src/keyboards/reply.py
@@ -4,7 +4,10 @@ main_kb = ReplyKeyboardMarkup(
 
     keyboard=
     [
-        [KeyboardButton(text='Смотреть анкеты 🔥'), KeyboardButton(text='Заполнить анкету заново 🔄'), KeyboardButton(text='Статистика 📊')]
+        [KeyboardButton(text='Смотреть анкеты 🔥')],
+        [KeyboardButton(text='Заполнить анкету заново 🔄')],
+        [KeyboardButton(text='Статистика 📊')],
+        [KeyboardButton(text='Отключить анкету 💤')]
     ],
     one_time_keyboard=True,
     resize_keyboard=True
@@ -13,7 +16,8 @@ main_kb = ReplyKeyboardMarkup(
 ank_kb = ReplyKeyboardMarkup(
     keyboard=
     [
-        [KeyboardButton(text='Лайк 👍'), KeyboardButton(text='Написать ✍️'), KeyboardButton(text='Дизлайк 👎'), KeyboardButton(text='Отключить анкету 💤')]
+        [KeyboardButton(text='Лайк 👍')],
+        [KeyboardButton(text='Дизлайк 👎')]
     ],
     resize_keyboard=True,
     input_field_placeholder="Выбери опцию ниже"
