1fd22f853f239c9902938a17a19f2b7aeaea50e8
youngrock1 <proger1311@gmail.com>
2026-02-04 22:36:00 +0300

    Update db.py

---
+++ src/database/db.py
@@ -0,0 +1,14 @@
+import asyncio
+import asyncpg
+
+async def main():
+
+    conn = await asyncpg.connect(
+        user='user',
+        password='password',
+        database='database',
+        host='localhost',
+        port=5432
+    )
+
+asyncio.run(main())
