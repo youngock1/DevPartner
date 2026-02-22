15970ec7e722b5b0d8afa9f189fba5c50d767349
youngrock1 <proger1311@gmail.com>
2026-02-22 12:18:34 +0300

    Update bot.py Добавил логирование, добавил обработчики типичных ошибок, прокоментировал код

--- src/bot.py
+++ src/bot.py
@@ -1,28 +1,40 @@
-from aiogram import Bot, Dispatcher
-from dotenv import load_dotenv
-import asyncio
-import logging
-import os
-from callbacks import callbacks
+from aiogram.exceptions import TelegramAPIError, TelegramBadRequest, TelegramNetworkError
 from handlers import bot_messages, user_commands, questionare
+from aiogram import Bot, Dispatcher
+from callbacks import callbacks
+from dotenv import load_dotenv
+import datetime, asyncio
+import logging, sys, os
 
-load_dotenv()
 
-
-bot = Bot(token=os.getenv("BOT_TEST_TOKEN"))
-dp = Dispatcher()
+load_dotenv() # Загружаем переменные окружения
 
 
 async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
-    dp.include_routers(
+
+    bot = Bot(token=os.getenv("BOT_TEST_TOKEN")) # Инициализация бота
+    dp = Dispatcher() # Инициализация диспетчера
+
+    dp.include_routers( # Подключение Роутеров(обработчиков)
         callbacks.router,
         user_commands.router,
         questionare.router,
-        bot_messages.router
-    )
-    await dp.start_polling(bot)
+        bot_messages.router)
+
+    logging.info(f"INFO-DevPartner | IT - is launched at {datetime.datetime.now().strftime("%Y/%m/%d  %H:%M:%S")}") # Логирование запуска бота
+    await bot.delete_webhook(drop_pending_updates=True) # Удаление обновлений за АФК бота
+    await dp.start_polling(bot) # Запуск polling бота
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        logging.basicConfig(level=logging.INFO, stream=sys.stdout) # Логирование всех обновлений в боте(абсолютно всех)
+        asyncio.run(main()) # Запуск бота
+    except TelegramNetworkError as error_network:
+        logging.info(f'Ошибка в подключении: {error_network}.')
+    except TelegramAPIError as error_api_connect:
+        logging.info(f'Ошибка в подключении API: {error_api_connect}.')
+    except TelegramBadRequest as error_bad_request:
+        logging.info(f'Некорректный запрос на сервер TG: {error_bad_request}.')
+    except KeyboardInterrupt:
+        logging.info(f'Прекращена работа программы сочетанием клавиш Cntrl + C.')
